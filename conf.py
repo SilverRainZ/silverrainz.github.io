1a9f7199eb2711c19bbeb63e8f1ce0c6afa77e98
Shengyu Zhang <i@silverrainz.me>
2020-06-19 13:19:38 +0800

    Add extension sphinxcontrib.email

--- conf.py
+++ conf.py
@@ -43,6 +43,8 @@ extensions.append('sphinx.ext.githubpages')
 
 extensions.append('sphinxcontrib.peopledomain')
 
+extensions.append('sphinxcontrib.email')
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
