d82be74714cb791d1bd70fd269d3c11e08198d9f
Shengyu Zhang <i@silverrainz.me>
2021-05-06 00:17:05 +0800

    2021-05-06 00:17

---
+++ notes/leetcode/n21-merge-two-sorted-lists/src/lib.rs
@@ -0,0 +1,32 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+
+pub struct Solution
+impl Solution {
+    pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
