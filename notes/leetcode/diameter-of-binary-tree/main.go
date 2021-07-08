efd37db9d7707092cae1d2541ba98c3f57481291
Shengyu Zhang <i@silverrainz.me>
2021-07-09 00:47:37 +0800

    2021-07-09 00:47

--- notes/leetcode/diameter-of-binary-tree/main.go
+++ notes/leetcode/diameter-of-binary-tree/main.go
@@ -1,70 +1,96 @@
 package main
 
 import (
-	"fmt"
 	"sort"
 )
 
-// Definition for a binary tree node.
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
-var max int
+var res int
 
 func maxInts(s ...int) int {
-	if len(s) == 5 {
-		fmt.Println("Before sort", s)
-		fmt.Println("After sort", s)
-	}
 	sort.Ints(s)
 	return s[len(s)-1]
 }
 
+// Return (left, right)
 func armSpan(root *TreeNode) (int, int) {
-	var ll, lr, rl, rr, L, R int
-	ints := []int{max}
+	var L, R int
+	ints := []int{res}
 	if root.Left != nil {
 		ll, lr := armSpan(root.Left)
 		L = maxInts(ll, lr) + 1
-		ints = append(ints, ll+lr)
 	}
 	if root.Right != nil {
 		rl, rr := armSpan(root.Right)
 		R = maxInts(rl, rr) + 1
-		ints = append(ints, rl+rr)
 	}
 
-	if root.Left != nil && root.Right != nil {
-		ints = append(ints, ll+rl+2, ll+rr+2, lr+rl+2, lr+rr+2)
-	}
-	max = maxInts(ints...)
-
+	ints = append(ints, L+R)
+	res = maxInts(ints...)
+	// println("node", root.Val, L, R)
 	return L, R
 }
 
 func diameterOfBinaryTree(root *TreeNode) int {
+	res = 0
 	armSpan(root)
-	return max
+	return res
+}
+
+// Return (left, right)
+func armSpan2(root *TreeNode) (int, int) {
+	var L, R int
+	if root.Left != nil {
+		ll, lr := armSpan2(root.Left)
+		L = maxInts(ll, lr) + 1
+	}
+	if root.Right != nil {
+		rl, rr := armSpan2(root.Right)
+		R = maxInts(rl, rr) + 1
+	}
+
+	ints := []int{res}
+	ints = append(ints, L+R)
+	res = maxInts(ints...)
+	// println("node", root.Val, L, R)
+	return L, R
 }
 
 func diameterOfBinaryTree2(root *TreeNode) int {
+	res = 0
+	armSpan2(root)
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+// Return (left, right)
+
+func diameterOfBinaryTree3(root *TreeNode) int {
+	res = 0
+	depth(root)
+	return res
+}
+
+func depth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	var l, r int
-	var ints []int
-	if root.Left != nil {
-		l = diameterOfBinaryTree2(root.Left)
-		ints = append(ints, l+1)
-	}
-	if root.Right != nil {
-		r = diameterOfBinaryTree2(root.Left)
-	}
-	max = maxInts(max)
-	return max
+	L := depth(root.Left)
+	R := depth(root.Right)
+	res = max(res, L+R)
+	return max(L, R) + 1
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
 func main() {
@@ -85,4 +111,17 @@ func main() {
 	}
 	println(diameterOfBinaryTree(t1))
 
+	t2 := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+		},
+		Right: &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val: 4,
+			},
+		},
+	}
+	println(diameterOfBinaryTree(t2))
 }
