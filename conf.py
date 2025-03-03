91e2437a3a201e787dc89a8567e6899df3a05aeb
Shengyu Zhang <i@silverrainz.me>
2025-03-03 22:51:37 +0800

    [conf] Fixup repository_branch

--- conf.py
+++ conf.py
@@ -139,6 +139,7 @@ html_favicon = '_static/favicon.png'
 html_theme = 'sphinx_book_theme'
 html_theme_options = {
     'repository_url': f'https://github.com/{author_id}/bullet',
+    'repository_branch': "master",
     'use_repository_button': True,
     'use_download_button': False,
     'use_edit_page_button': False,
