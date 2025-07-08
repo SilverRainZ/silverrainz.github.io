9d36cfdeb358a15a7594807339276329f52796c7
Shengyu Zhang <i@silverrainz.me>
2025-07-08 23:24:23 +0800

    [conf] Replace _images, _download with symlinks

--- _conf/fetch_artwork.py
+++ _conf/fetch_artwork.py
@@ -15,7 +15,7 @@ def fetch_artwork_filter(env: BuildEnvironment):
     """
 
     def _filter(id_: str) -> str | None:
-        imgdir = '_blob/artworks'
+        imgdir = '_blobs/artworks'
         imgdir = env.srcdir.joinpath(imgdir)
         result = subprocess.run(['/home/la/latree/bin/artworks-fetch', id_, imgdir])
         if result.returncode != 0:
